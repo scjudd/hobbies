bffb126a9623e725f8bded2383516662ae4042d3
Spencer Judd <scj@foxrunn.net>
2010-08-01 12:13:36 -0400

    fixed multi-line dailybooth blurbs

--- scrapers/DailyboothScraper.py
+++ scrapers/DailyboothScraper.py
@@ -14,6 +14,6 @@ class DailyboothScraper(object):
         for image in image_list:
             item = Item()
             item.url = image.xpath('div[1]/a[1]/img/@src')[0]
-            item.blurb = image.xpath('div[2]/div[2]/p/text()')[0]
+            item.blurb = image.xpath('div[2]/div[2]/p/text()')
             items.append(item)
         return items
